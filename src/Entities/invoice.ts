64b198564b2b01345e07e4b8c5cf0c9d9ce83111
FernandoCalla <fernandorafaelcallayarihuaman@gmail.com>
2022-09-30 17:37:28 -0500

    Se desarrollo la entidad invoice

---
+++ src/Entities/invoice.ts
@@ -0,0 +1,17 @@
+import {BaseEntity, Column, CreateDateColumn, Entity, JoinColumn, OneToMany, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm'
+import { Reservation } from './reservation';
+import { ReservationRoom } from './reservationRoom'
+@Entity()
+export class Invoice extends BaseEntity{
+    @PrimaryGeneratedColumn()
+    id : number
+    @Column()
+    totalPrice:number
+    @CreateDateColumn()
+    createdAt: Date;
+    @UpdateDateColumn()
+    updatedAt:Date;
+    @OneToOne(() => Reservation,(reservation) => reservation.invoice)
+    @JoinColumn()
+    reservation: Reservation
+}
